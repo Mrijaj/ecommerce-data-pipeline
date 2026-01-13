badafaaa90467bc4c9654655c722383d07f16b0a
MRIJAZ404 <ijaz04186@gmail.com>
2026-01-13 19:19:35 +0530

    Remove generated data directories from version control

---
+++ kafka_client/producer_orders.py
@@ -0,0 +1,17 @@
+import json
+import time
+from kafka import KafkaProducer
+from data_generator.utils import generate_order
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+print("Kafka Order Producer started...")
+
+while True:
+    order = generate_order()
+    producer.send("orders", order)
+    print(f"Sent: {order}")
+    time.sleep(2)
